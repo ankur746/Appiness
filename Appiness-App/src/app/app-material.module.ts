34d0d6856cc9c2b7bde8691658a2582d9b9981f1
Ankur Parmar <ankur@shankventures.com>
2019-06-07 10:58:09 +0530

    date picker

--- Appiness-App/src/app/app-material.module.ts
+++ Appiness-App/src/app/app-material.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatSelectModule } from '@angular/material';
+import { MatInputModule, MatSelectModule, MatNativeDateModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule
+    CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule
   ],
-  exports: [MatFormFieldModule, MatInputModule, MatSelectModule]
+  exports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule ]
 })
 export class AppMaterial { }
